d2ceb664dbb133d26aa9ba4da470340dce49a970
mgaur10 <46466824+mgaur10@users.noreply.github.com>
2022-09-30 13:25:13 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,38 +1,51 @@
 /**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-         
-     
+* Copyright 2022 Google LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+
+## This provides PoC demo environment for Cloud IDS
+## NOTE this is not built for production workload ##
+
+
 
 variable organization_id {
 }
-variable billing_account {    
-}
-variable folder_name {
-}
 
-variable demo_project_id {
+
+variable folder_name {
 }
 
 variable vpc_network_name {
 }
 
-variable ids_network_zone{
+variable network_zone{
 
 }
 
-variable ids_network_region {
-    
+variable network_region {
+
+}
+
+
+
+variable billing_account {    
+}
+
+variable demo_project_id {
+}
+
+variable proxy_access_identities {
+
 }
